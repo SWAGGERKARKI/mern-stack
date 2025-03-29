73e52950129b7b5c4bfcb1bac11b5de484494f79
Sagar Karki <sagarkarki995@gmail.com>
2025-03-29 11:13:29 +0545

    Create Page Wrapper Component for Animation

---
+++ src/components/PageWrapper.jsx
@@ -0,0 +1,22 @@
+import { motion } from "motion/react";
+
+function PageWrapper({ children }) {
+  const pageVariant = {
+    initial: { opacity: 0, y: -20 },
+    animate: {opacity: 1, y: 0, transition: { duration: 0.5 } },
+    exit: { opacity: 0, y: 20, transition: { duration: 0.3 } },
+  };
+
+  return(
+    <motion.div
+      variants={pageVariant}
+      initial={initial}
+      animate={animate}
+      exit={exit}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+export default PageWrapper;
